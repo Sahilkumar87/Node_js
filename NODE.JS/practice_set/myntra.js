d398321ab6928fe5ff5504a50280d6fbf37b7792
sahil kumar <sahilkumar06804@gmail.com>
2026-06-27 00:23:09 +0530

    myntra project complete and 4.response_request bhi done!

--- NODE.JS/practice_set/myntra.js
+++ NODE.JS/practice_set/myntra.js
@@ -1,44 +1,36 @@
-const http = require('http');
+const http = require("http");
 
 const reqServer = (req, res) => {
-    console.log(req.url, req.method, req.headers);
+  console.log(req.url, req.method, req.headers);
 
-    if(req.url === '/'){
-        res.setHeaders('Content-Type', 'text/html');
-        res.write("<html>");
-        res.write("<head><title>Myntra App</title></head>");
-        res.write("<body>");
-        res.write("<h1>Welcome to Home</h1>");
-        res.write("</body>")
-        res.write("</html>");
-        return res.end();
-    }
-    else if(req.url.toLowerCase() === '/men'){
-        res.setHeaders('Content-Type', 'text/html');
-        res.write("<html>");
-        res.write("<head><title>Myntra App</title></head>");
-        res.write("<body>");
-        res.write("<h1>Welcome to Men</h1>");
-        res.write("</body>")
-        res.write("</html>");
-        return res.end();
-
-    }
-    else if(req.url.toLowerCase() === "/women"){
-        
-        res.setHeaders('Content-Type', 'text/html');
-        res.write("<html>");
-        res.write("<head><title>Myntra App</title></head>");
-        res.write("<body>");
-        res.write("<h1>Welcome to Women</h1>");
-        res.write("</body>")
-        res.write("</html>");
-        return res.end();
-    }
-}
+  if (req.url === "/home") {
+    res.setHeader("Content-Type", "text/html");
+    res.write(
+      '<html><head><title>Myntra App</title></head><body><nav><ul><li><a href="/home">Home</a></li><li><a href="/men">Men</a></li><li><a href="/women">Women</a></li><li><a href="/kids">Kids</a></li><li><a href="/cart">Cart</a></li></ul></nav></body></html>',
+    );
+    return res.end();
+  }
+  if (req.url.toLowerCase() === "/men") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<h1>Welcome to Men</h1>");
+    return res.end();
+  } else if (req.url.toLowerCase() === "/women") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<h1>Welcome to Women</h1>");
+    return res.end();
+  } else if (req.url.toLowerCase() === "/kids") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<h1>Welcome to Kids</h1>");
+    return res.end();
+  } else if (req.url.toLowerCase() === "/cart") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<h1>Welcome to Cart</h1>");
+    return res.end();
+  }
+};
 
 const server = http.createServer(reqServer);
 const port = 12121;
 server.listen(port, () => {
-    console.log(`server is running at port http://localhost:${port}`);
+  console.log(`server is running at port http://localhost:${port}`);
 });
