932173b1c33252e9715ab9f3d03b217a8b6eff4b
sahil kumar <sahilkumar06804@gmail.com>
2026-06-28 22:07:52 +0530

    lec-5 using modules tak complete

---
+++ NODE.JS/5.Parsing_Request/server.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const requestHandler= require('./user');
+
+
+const server = http.createServer(requestHandler);
+
+const port = 55555;
+server.listen(port, () => {
+    console.log(`Server running on address http://localhost:${port}`);
+})
