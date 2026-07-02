c516a3dd380c49e7afc7a64d0f33a8988e70dd54
sahil kumar <sahilkumar06804@gmail.com>
2026-07-02 23:32:22 +0530

    spelling mistake

--- NODE.JS/practice_set/ex-project/server.js
+++ NODE.JS/practice_set/ex-project/server.js
@@ -1,23 +1,48 @@
-const http = require('http');
 const express = require('express');
-
 const app  = express();
-app.get("/", (req, res, next) => {
-    console.log("home page");
-    res.send(`<p>came from first</p>`);
+
+
+app.use((req, res, next) => {
+    console.log("first dummy Middleware", req.url, req.method);
+    // res.send(`<p>came from first</p>`);
     next();
+});
+
+app.use((req, res, next) => {
+    console.log("second dummy Middleware", req.url, req.method);
+    // res.send(`<p>came from second</p>`);
+    next();
+});
+
+// app.use("/", (req, res, next) => {
+//     console.log("third middleware", req.url, req.method);
+//     // res.send("<h1>came from third</h1>");
+//     next();
+// });
+
+
+app.get("/", (req, res, next) => {
+    console.log("Handling / for GET", req.url, req.method);
+    res.send("<h1>Welcome to complete coding</h1>");
+});
+
+
+
+
+app.get("/contact-us", (req, res, next) => {
+    console.log("handling / contact-us for get", req.url, req.method);
+    res.send(`<h1>please give your details</h1>
+        <form action="/contact-us" method="POST">
+        <input type="text" name="name" placeholder="Enter your name"/>
+        <input type="email" name="email" placeholder="Enter your Email"/>
+        <input type="Submit"/>
+        </form>
+        `);
 });
 
 app.post("/contact-us", (req, res, next) => {
-    console.log("contact page");
-    res.send("<p>came from second</p>");
-    next();
-});
-
-app.use("/about-us", (req, res, next) => {
-    console.log("about page");
-    res.send("<p>came from third</p>");
-    next();
+     console.log("handling / contact-us for POST", req.url, req.method);
+     res.send(`<h1>thanks for your details</h1>`);
 });
 
 
@@ -25,11 +50,7 @@ app.use("/about-us", (req, res, next) => {
 
 
 
-const port = 12323;
+const port = 1234;
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
-})
-
-const reqserver = (req, res, next) => {
-
-}
+});
