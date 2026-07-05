3e95b41d831a8ba213299ead83d77bfc0734b60f
sahil kumar <sahilkumar06804@gmail.com>
2026-07-05 22:14:57 +0530

    routing and html ka file import export or file structure samjh aa gya

--- NODE.JS/11.airbnb/app.js
+++ NODE.JS/11.airbnb/app.js
@@ -1,9 +1,14 @@
+//core module
+const path = require('path');
+
+
 // external module
 const express = require('express');
 
 //local module
 const userRouter = require('./routes/userRouter');
 const hostRouter = require('./routes/hostRouter');
+const rootDir = require('./utils/pathUtils');
 
 const app = express();
 
@@ -12,10 +17,18 @@ app.use((req, res, next) => {
     next();
 });
 
+
+
 app.use(express.urlencoded());
 
 app.use(userRouter);
-app.use(hostRouter)
+app.use("/host",hostRouter);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(rootDir, 'views', '404page.html'));
+        });
+
+
 
 
 
--- NODE.JS/11.airbnb/routes/hostRouter.js
+++ NODE.JS/11.airbnb/routes/hostRouter.js
@@ -1,24 +1,18 @@
+// core module
+const path = require('path');
+
 const express = require('express');
 const hostRouter = express.Router();
 
 
-hostRouter.get("/host/add-home", (req, res, next) => {
-    res.send(`
-        <h1>Register Your home here: </h1>
-        <form action="/host/add-home" method="POST">
-        <input type="text" name="houseName" placeholder="Enter your house Name: "/>
-        <input type="submit"/>
-        </form>
-        `);
+hostRouter.get("/add/home", (req, res, next) => {
+    res.sendFile(path.join(__dirname, "../", 'views', 'addHome.html'));
 })
 
 
-hostRouter.post("/host/add-home", (req, res, next) => {
+hostRouter.post("/add/home", (req, res, next) => {
     console.log(req.body)
-    res.send(`
-        <h1>Home Registered succesfully</h1>
-        <a href="/">Go To Home</a>
-        `);
+    res.sendFile(path.join(__dirname, '../', 'views', 'successfullyAdd.html'));
 })
 
 module.exports = hostRouter;
