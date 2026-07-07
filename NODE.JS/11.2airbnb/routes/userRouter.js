df0c58f31c520bd68af29c9402abdea61c498455
sahil kumar <sahilkumar06804@gmail.com>
2026-07-07 22:00:31 +0530

    recreate with new concept

---
+++ NODE.JS/11.2airbnb/routes/userRouter.js
@@ -0,0 +1,14 @@
+// core module
+const path = require('path');
+// external module
+const express = require('express');
+const userRouter = express.Router();
+
+//local module
+const rootDir = require('../utils/pathUtils');
+
+userRouter.get("/", (req, res, next) => {
+    res.sendFile(path.join(rootDir, "views", "home.html"));
+});
+
+module.exports = userRouter;
