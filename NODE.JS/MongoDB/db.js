1007f4485c0e92559f289b5c7daaa00e56977d15
sahil kumar <sahilkumar06804@gmail.com>
2026-08-02 22:38:06 +0530

    using mongoSH

--- NODE.JS/MongoDB/db.js
+++ NODE.JS/MongoDB/db.js
@@ -5,3 +5,12 @@ R -> read
 U -> update
 D -> delete
  */
+
+
+{
+    "id" :  "12443132323",
+    "name": "sahil",
+    "message": "please bring my order",
+    "timestamp": 02/08/2026 10:33PM,
+
+} 
