ea690efa16d5349d59ffd2bf129acfc5572ebc5c
sahil kumar <sahilkumar06804@gmail.com>
2026-08-13 23:38:08 +0530

    adding postLogin function

--- NODE.JS/17. cookies and session/controllers/authController.js
+++ NODE.JS/17. cookies and session/controllers/authController.js
@@ -5,3 +5,12 @@ exports.getLogin = (req, res, next) => {
     editing: false,
   });
 };
+
+
+exports.postLogin = (req, res, next) => {
+  res.render("host/edit-home", {
+    pageTitle: "Add Home to airbnb",
+    currentPage: "addHome",
+    editing: false,
+  });
+}
